19bfe70c064168aa421c497a756addd8c541a0b8
mzapataj <mzapataj@uninorte.edu.co>
2019-11-03 20:57:23 -0500

    sessions_controller.rb corrected

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -26,34 +26,35 @@ module Api::V1
         end
     end
 =end
-class SessionsController < ApplicationController
-    def new
-    
-    end
-
-    def create
-#        puts("_#{params[:user][:email]}_#{params[:user][:password]}")
-        @user = User.find_by_email(params[:email])
-        if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
-            #puts params[:name]
-            render json: @user, status: :created
-        else
-            render json: 
+    class SessionsController < ApplicationController
+        def new
+        
         end
-    end
 
-    def destroy
-        @session[:user_id] = nil
-        render json: @session
-    end
-
-
-    private 
-        # Only allow a trusted parameter through.
-        def user_params
-            params.require(:user).permit(:email, :password)
+        def create
+    #        puts("_#{params[:user][:email]}_#{params[:user][:password]}")
+            @user = User.find_by_email(params[:email])
+            if @user && @user.authenticate(params[:password])
+                session[:user_id] = @user.id
+                #puts params[:name]
+                render json: @user, status: :created
+            else
+                render json: 
+            end
         end
-    end
 
+        def destroy
+            @session[:user_id] = nil
+            render json: @session
+        end
+
+
+        private 
+            # Only allow a trusted parameter through.
+            def user_params
+                params.require(:user).permit(:email, :password)
+            end
+        end
+
+    end
 end
